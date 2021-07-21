1d3e90d561592a405a6d4f7f96cc8f57b591eceb
Erich Purpur <epurpur@gmail.com>
2021-07-21 11:13:17 -0400

    building homepage items and styles

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,15 +1,28 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom';
+
 /* CSS styles */
-import '../App.css'
 import '../styles/HomepageStyle.css'
-
-
 
 const Homepage = () => {
     return (
-        <section id="homePage"> 
-            <h1>Connecting construction jobs and labor. Faster.</h1>
+        <section id="homePageTop"> 
+            <h1>Connecting construction jobs and labor. <span id="faster">Faster.</span></h1>
+            <section id="register">
+                New User?
+            </section>
+            <section id="returning">
+                <div id="returnContractor">
+                    <p>Returning Contractor?</p>
+                    <p>Login</p>
+                </div>
+                <div id="returnEmployee">
+                    <p>Returning Employee?</p>
+                    <p>Login</p>
+                </div>
+            </section>
+            
         </section>
     )
 }
