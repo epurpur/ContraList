aeb1e189d37e9c29352cb67aee8686fa0680d308
Erich Purpur <epurpur@gmail.com>
2021-07-21 18:11:19 -0400

    creating contractor registration form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+
+/* CSS styles */
+import '../styles/Registration.css';
+
 
 const Register = ( {userType} ) => {
 
@@ -7,13 +11,26 @@ const Register = ( {userType} ) => {
     return (
         <div>
             {/* Either show contractor or worker registration form depending on value of userType */}
+            {/* Contractor Registration form */}
             {userType === 'contractor' ? 
             
-            <div id="contractorRegistrationForm">
+            <section id="contractorRegistrationForm">
                 <h1 id='contractorTitle'>Register as a licensed contractor</h1>
-                <p>Another</p>
-            </div> 
+                <form>
+                    <label for="fname">First name:</label>
+                    <input type="text" />
+                    <label for="lname">Last name:</label>
+                    <input type="text"/>
+                    <label for="email">Email:</label>
+                    <input type="email"/>
+                    <label for="lname">Phone:</label>
+                    <input type="phone"/>
+                    <label for="lname">Contractor License #:</label>
+                    <input type="text"/>
+                </form>
+            </section> 
             
+            // Worker registration form
             : <div>Worker Registration</div>
             
             }
