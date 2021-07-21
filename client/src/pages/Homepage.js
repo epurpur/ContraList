b280a38ac80e550fe26a10a90134f706fa4dcd82
Erich Purpur <epurpur@gmail.com>
2021-07-21 10:44:13 -0400

    messed up css

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
 const Homepage = () => {
     return (
-        <section style={{ textAlign: "center" }}>
-            <div id="homepageTop" >
-                <h1 style={{ "font-size": "80px" }}>Connecting construction jobs and labor. <span className='text-emphasis-orange'>Faster.</span></h1>
-            </div>
-            <div id='homepageMiddle' style={{ "font-size": "40px" }}>
-                New User ? <Link className="inline-button text-emphasis" 
-                style={{ "background-color" : "var(--background-header)",
-                    "border-radius" : "10px",
-                    "padding": "10px",
-                    "text-decoration" : "none"  }}>Register</Link>
-            </div>
-        </section> 
-
-        
-
+        <section id="homePage"> 
+            <h1>Connecting construction jobs and labor. Faster.</h1>
+        </section>
     )
 }
 
-export default Homepage
+export default Homepage;
