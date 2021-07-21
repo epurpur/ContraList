283cf70d1370b3e2e7ef824b25164bcbdf9210f8
Erich Purpur <epurpur@gmail.com>
2021-07-21 07:58:48 -0400

    adding styles and content for home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,31 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 
-    const goHome = () => {
-        // redirects to home page
-        window.location = '/';
-    }
-
-    const contractorLogin = () => {
-        // redirects to contractor login page
-        window.location = '/ContractorLogin';
-    }
-
-    const employeeLogin = () => {
-        // redirects to employee login page
-        window.location = '/EmployeeLogin';
-    }
-
     return (
-        <header id='header'> 
-            <h1 id="logo">Contra<span id='logo2'>List</span></h1>
+        <header> 
+            <h1 id="logo">Contra<span className='text-emphasis'>List</span></h1>
             <section id="navButtons">
                 <Link to='/' id="testing" className="navButton" style={{ textDecoration: 'none' }}>Home</Link>
                 <Link to='/ContractorLogin' className="navButton" style={{ textDecoration: 'none' }}>Contractor Login</Link>
                 <Link to='/EmployeeLogin' className="navButton" style={{ textDecoration: 'none' }}>Employee Login</Link>
-                {/* <button className="navButton" onClick={goHome}> Home </button>
-                <button className="navButton" onClick={contractorLogin}> Contractor Login </button>
-                <button className="navButton" onClick={employeeLogin}> Employee Login </button> */}
             </section>
         </header>
     )
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
     return (
-        <div>
-            Homepage
-        </div>
+        <section style={{ textAlign: "center" }}>
+            <div id="homepageTop" >
+                <h1 style={{ "font-size": "80px" }}>Connecting construction jobs and labor. <span className='text-emphasis-orange'>Faster.</span></h1>
+            </div>
+            <div id='homepageMiddle' style={{ "font-size": "40px" }}>
+                New User ? <Link className="inline-button text-emphasis" 
+                style={{ "background-color" : "var(--background-header)",
+                    "border-radius" : "10px",
+                    "padding": "10px",
+                    "text-decoration" : "none"  }}>Register</Link>
+            </div>
+        </section> 
+
+        
+
     )
 }
 
