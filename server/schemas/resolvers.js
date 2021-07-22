cf716798cfe69a2f0617a945e01c2828c9b77d72
Shaima Jobran <shaimajobran22@gmail.com>
2021-07-22 10:09:14 -0400

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,17 @@
-const { Contractor } = require('../models');
+const { Contractor,Worker } = require('../models');
 
 const resolvers = {
     Query: {
         contractors: async () => {
             return await Contractor.find({})
-        }
+        },
+        worker:async (parent,{_id})=>{
+            const params =_id ? {_id} : {};
+            return Worker.find(params);
+        },
+    },
+    Mutation:{
+        
     }
 };
 
