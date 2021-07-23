869b7da46f6e13ce00b9676d91993ab83f76cac4
Erich Purpur <epurpur@gmail.com>
2021-07-23 09:10:09 -0400

    adding new Job submission form

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -6,10 +7,13 @@ import LoginForm from '../../components/LoginForm';
 
 const Login = ( {userType} ) => {
     
+    //WILL MAKE DB CALL HERE FOR USER BY EMAIL. I THINK THIS IS WHERE I PASS THE PROP OF 'userInfo' AND ALSO 'setUserInfo'
+
     return (
         <div>
             {/* Just show generic login form. userType will be determined upon login */}
             <LoginForm userType={userType} />
+            <Link to='LandingPage'>Temporary link to LandingPage</Link>
         </div>
     )
 }
--- client/src/pages/NewJob/index.js
+++ client/src/pages/NewJob/index.js
@@ -1,13 +1,29 @@
 import React, {useState} from 'react'
 
+import './styles.css';
+
 const NewJob = () => {
 
     //NEED TO GET INFO ABOUT CONTRACTOR AND PASS INTO NEW JOB CARD
 
     return (
-        <div>
-            Create new job
-        </div>
+        <section id="newJob">
+            <h1 id='contractorTitle'>Create new job</h1>
+            <form id="newJobForm">
+                <label for="title" required>Job Title:</label>
+                <input type="text" />
+                <label for="description" required>Job Description:</label>
+                <input type="text"/>
+                <label for="location" required>Job Location:</label>
+                <input type="text"/>
+                <label for="duration">Duration:</label>
+                <input type="text"/>
+                <label for="comments">Other Comments:</label>
+                <textarea></textarea>
+                <button type="submit" form="nameform" value="Submit">Submit</button>
+            </form>
+
+        </section>
     )
 }
 
