10745922f82eb818cb7ac618e1f8a037e24258ac
Erich Purpur <epurpur@gmail.com>
2021-07-23 09:37:42 -0400

    modifying job card and styles

--- client/src/components/JobCard/index.js
+++ client/src/components/JobCard/index.js
@@ -5,15 +5,27 @@ const JobCard = () => {
         <div id="jobCardsHolder">
             <div className='jobCard'>
                 <p>Job 1</p>
-                <p> Short Description </p>
+                <p> Description </p>
+                <div className='jobCardBottom'>
+                    <p>Created by: 'contractor name'</p>
+                    <p>Date: 7/22/2021</p>
+                </div>
             </div>
             <div className='jobCard'>
                 <p>Job 2</p>
                 <p> Short Description </p>
+                <div className='jobCardBottom'>
+                    <p>Created by: 'contractor name'</p>
+                    <p>Date: 7/23/2021</p>
+                </div>
             </div>            
             <div className='jobCard'>
                 <p>Job 3</p>
                 <p> Short Description </p>
+                <div className='jobCardBottom'>
+                    <p>Created by: 'contractor name'</p>
+                    <p>Date: 7/24/2021</p>
+                </div>
             </div>        
         </div>
     )
