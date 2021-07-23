96759ad57e9d1420f09de7205e385e962df11ef9
Shaima Jobran <shaimajobran22@gmail.com>
2021-07-23 11:57:36 -0400

    fixed graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,15 +4,35 @@ const typeDefs = gql`
 
     type Contractor {
         _id: ID
-        name: String!
-        email: String!
-        password:String!
-        phoneNumber: String!
+        name: String
+        email: String
+        password:String
+        phoneNumber: Int
+        licenseNumber:Int
+     
     }
+     type Worker{
+         _id:ID
+         name:String
+         email:String
+         password:String
+         phoneNumber:Int
+       
+     
+
+     }
+     type Job{
+         _id:ID
+         jobText:String
+         jobAuthor:String
+         createdAt:String
+
+     }
 
     type Query {
-        contractors: [Contractor]
+        contractor: [Contractor]
     }
+    
   
 
 `;
