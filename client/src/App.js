dc9f16a7a6645cb90430dc4bab0f01c982f173f9
Erich Purpur <epurpur@gmail.com>
2021-07-24 11:19:17 -0400

    adding apollo into react front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 /* CSS styles */
 import './App.css';
@@ -15,42 +16,51 @@ import SingleJobPage from './pages/SingleJobPage';
 /* Components */
 import Header from './components/Header';
 
+/* Apollo Setup */
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+
 function App() {
 
   // defining empty user type. Will be set to 'contractor' or 'worker' depending on what user chooses
   const [userType, setUserType] = useState('');
 
   return (
-    <Router>
-      <div className="App">
-        <Header userType={userType} setUserType={setUserType}/>
-        
-        {/* Establishing routes to all endpoints */}
-        <Switch>
-          <Route exact path="/">
-            <Homepage userType={userType} setUserType={setUserType} />
-          </Route>
-          <Route exact path="/Login" >
-            <Login userType={userType} setUserType={setUserType} />
-          </Route>          
-          <Route exact path="/Register" >
-            <Register userType={userType} />
-          </Route>
-          <Route exact path="/LandingPage" >
-            <LandingPage userType={userType} setUserType={setUserType} />
-          </Route> 
-          <Route exact path="/NewJob" >
-            <NewJob />
-          </Route>    
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          <Header userType={userType} setUserType={setUserType}/>
+          
+          {/* Establishing routes to all endpoints */}
+          <Switch>
+            <Route exact path="/">
+              <Homepage userType={userType} setUserType={setUserType} />
+            </Route>
+            <Route exact path="/Login" >
+              <Login userType={userType} setUserType={setUserType} />
+            </Route>          
+            <Route exact path="/Register" >
+              <Register userType={userType} />
+            </Route>
+            <Route exact path="/LandingPage" >
+              <LandingPage userType={userType} setUserType={setUserType} />
+            </Route> 
+            <Route exact path="/NewJob" >
+              <NewJob />
+            </Route>    
 
-          {/* This URL will eventually be changed to /SingleJobPage/:id */} 
-          <Route exact path="/SingleJobPage" >
-            <SingleJobPage userType={userType} />
-          </Route>    
-        </Switch>
-        
-      </div>
-    </Router>
+            {/* This URL will eventually be changed to /SingleJobPage/:id */} 
+            <Route exact path="/SingleJobPage" >
+              <SingleJobPage userType={userType} />
+            </Route>    
+          </Switch>
+          
+        </div>
+      </Router>
+    </ApolloProvider>
 
   );
 }
