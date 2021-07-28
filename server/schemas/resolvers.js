2bcc48acc2acef5ef6e4d0c4c91c0bff0ed98926
Shaima Jobran <shaimajobran22@gmail.com>
2021-07-27 21:29:43 -0400

    changed models and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,49 +1,38 @@
 // const { AuthenticationError } = require('apollo-server-express');
-const { Contractor,Worker,Job} = require('../models');
+const { User,Job} = require('../models');
+// const { populate } = require('../models/');
 // const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        //finding all contractors
-        contractor: async () => {
-            return await Contractor.find({}).populate('job')
+        //finding all users
+        user: async () => {
+            return await User.find({}).populate('job')
         },
-        //finding all workers
-        worker: async () => {
-            return await Worker.find({}).populate('job')
-        },
+    
         //finding all jobs
         job: async () => {
-            return await Job.find({}).populate('contarctor','worker').populate({
-                path: 'worker',
-                populate: 'contractor'
-            })
+            return await Job.find({}).populate('user')
         },
-        // jobs: async (parent, args) => {
-        //     return await Class.findById(args.id);
-        //   },
+         //finding one user that applied and display the jobs that are connected to this user (contractor)
+         contractorjobs: async (parent, { user }) => {
+            // return Job.find({ user })
+            return Job.find({ _id: userId });
+          },
+      
     },
     Mutation:{
         createJob: async (parent,{jobText,jobAuthor,createdAt})=>{
             const job = await Job.create({jobText,jobAuthor,createdAt});
             return job;
         },
-        createContractor:async (parent,{name,email,password,phoneNumber,licenseNumber})=>{
-            const contractor = await Contractor.create({name,email,password,phoneNumber,licenseNumber});
-            return contractor;
+        createUser:async (parent,{name,email,password,phoneNumber,licenseNumber,roleId,description})=>{
+            const user = await User.create({name,email,password,phoneNumber,licenseNumber,roleId,description});
+            return user;
         },
+        
 
-        createWorker:async (parent,{name,email,password})=>{
-            const worker= await Worker.create({name,email,password});
-            return worker;
-        }
-        // submitApplictaion: async (parent,{ name,email})=>{
-        //     const apply =await Job.findOneAndUpdate(
-        //         {name},
-        //         {email}
-        //     );
-        //     return apply;
-        // },
+    
     },
 };
 
