f82347ff324359973e4f51b4f5fb1196e6d2d1f0
Shaima Jobran <shaimajobran22@gmail.com>
2021-07-28 22:00:09 -0400

    tried to fix loginauth

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// cosn  tbcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/contralist', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
