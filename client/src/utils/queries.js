1a08f155e11d788ce74e28c95a06ac591f640974
Shaima Jobran <shaimajobran22@gmail.com>
2021-07-29 19:51:07 -0400

    queries for client

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,47 @@
 import { gql } from '@apollo/client';
 
-// this query gets all workers from DB
-export const QUERY_ALL_WORKERS = gql`
-    query {
-        worker {
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      jobs {
         _id
-        name
-        email
-        phoneNumber
-        password
-        }
+        jobText
+        createdAt
+      }
     }
+  }
 `;
+
+export const QUERY_JOBS = gql`
+  query getJobs {
+    jobs {
+      _id
+      jobText
+      jobAuthor
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_JOB = gql`
+  query getSingleJob($jobId: ID!) {
+    job(jobId: $jobId) {
+      _id
+      jobText
+      jobAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
+
 
 
 
