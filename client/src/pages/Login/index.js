1d1155150353d32fc9f05ec84527b00fea73af1a
Erich Purpur <epurpur@gmail.com>
2021-07-31 07:45:20 -0400

    removing temporary link to landing page

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -7,13 +7,10 @@ import LoginForm from '../../components/LoginForm';
 
 const Login = ( {userType} ) => {
     
-    //WILL MAKE DB CALL HERE FOR USER BY EMAIL. I THINK THIS IS WHERE I PASS THE PROP OF 'userInfo' AND ALSO 'setUserInfo'
-
     return (
         <div>
             {/* Just show generic login form. userType will be determined upon login */}
             <LoginForm userType={userType} />
-            <Link to='LandingPage'>Temporary link to LandingPage</Link>
         </div>
     )
 }
