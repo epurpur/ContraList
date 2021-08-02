51d2e3f1f101cfc0f9c54953185cf76628744392
Erich Purpur <epurpur@gmail.com>
2021-08-02 14:07:59 -0400

    styling addJob form

--- client/src/pages/NewJob/index.js
+++ client/src/pages/NewJob/index.js
@@ -4,22 +4,69 @@ import './styles.css';
 
 const NewJob = () => {
 
+    const userId = localStorage.getItem('userId')
+
+    const [ jobInfo, setJobInfo ] = useState({ 
+        jobText: '', 
+        jobAuthor: userId,
+        location: '',
+        duration: '',
+        otherComments: '', 
+    });
+
+    //update state based on form input changes
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+
+        // spreads apart userInfo and sets name: value into it
+        setJobInfo({
+            ...jobInfo,
+            [name]: value,
+        });
+        console.log(jobInfo)
+    };
+
     //NEED TO GET INFO ABOUT CONTRACTOR AND PASS INTO NEW JOB CARD
 
     return (
         <section id="newJob">
             <h1 id='contractorTitle'>Create new job</h1>
             <form id="newJobForm">
-                <label for="title" required>Job Title:</label>
-                <input type="text" />
-                <label for="comments">Job Description:</label>
-                <textarea></textarea>
-                <label for="location" required>Job Location:</label>
-                <input type="text"/>
-                <label for="duration">Duration:</label>
-                <input type="text"/>
-                <label for="comments">Other Comments:</label>
-                <textarea></textarea>
+                <label>Job Description:</label>
+                <textarea
+                    placeholder="Write a short description about your job"
+                    name="jobText"
+                    value={jobInfo.jobText}
+                    onChange={handleChange}
+                    type="text" 
+                    required 
+                />
+                <label>Job Location:</label>
+                <input
+                    placeholder="Address or Location of job" 
+                    name="location"
+                    value={jobInfo.location}
+                    onChange={handleChange}
+                    type="text"
+                    required
+                />
+                <label>Duration:</label>
+                <input 
+                    placeholder="Date or time of job"
+                    name="duration"
+                    value={jobInfo.duration}
+                    onChange={handleChange}
+                    type="text"
+                    required
+                />
+                <label>Other Comments:</label>
+                <textarea
+                    placeholder="Any other information you want to add"
+                    name="otherComments"
+                    value={jobInfo.otherComments}
+                    onChange={handleChange}
+                    type="text"    
+                />
                 <button type="submit" form="nameform" value="Submit">Submit</button>
             </form>
         </section>
